4890393ddec980a319dd1cae8a6a033421248c03
QuentinAd <qadam2602@gmail.com>
2025-09-09 11:23:51 -0600

    feat: add timestamp to time col

--- src/utils/datetime_utils.py
+++ src/utils/datetime_utils.py
@@ -1,17 +1,43 @@
-from pyspark.sql.functions import to_date, col
+from pyspark.sql import functions as F
+from pyspark.sql.dataframe import DataFrame
 
-def timestamp_to_date_col(spark, df, timestamp_col, output_col):
+
+def timestamp_to_date_col(
+    df: DataFrame,
+    timestamp_col: str,
+    output_col: str,
+) -> DataFrame:
     """
-    Extracts the date from a timestamp column and adds it as a new column in the DataFrame.
-    
+    Extracts the date from a timestamp column and adds it as a new column.
+
     Parameters:
         spark: Spark Session.
         df (DataFrame): Input PySpark DataFrame containing the timestamp.
         timestamp_col (str): The name of the column containing the timestamp.
         output_col (str): The name for the output column with the date.
-    
+
     Returns:
         DataFrame: A new DataFrame with the additional date column.
     """
-    # Use to_date to extract the date part of the timestamp
-    return df.withColumn(output_col, to_date(col(timestamp_col)))
+    # Extract the date part of the timestamp
+    return df.withColumn(output_col, F.date_format(F.col(timestamp_col), 'yyyy-MM-dd'))
+
+
+def timestamp_to_time_col(
+    df: DataFrame,
+    timestamp_col: str,
+    output_col: str,
+) -> DataFrame:
+    """
+    Extracts the time (HH:mm:ss) from a timestamp column and adds it as a new column.
+
+    Parameters:
+        spark: Spark Session.
+        df (DataFrame): Input PySpark DataFrame containing the timestamp.
+        timestamp_col (str): The name of the column containing the timestamp.
+        output_col (str): The name for the output column with the time string.
+
+    Returns:
+        DataFrame: A new DataFrame with the additional time column.
+    """
+    return df.withColumn(output_col, F.date_format(F.col(timestamp_col), 'HH:mm:ss'))
